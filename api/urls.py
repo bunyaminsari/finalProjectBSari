3b96dd2e76e1ecd26abba41482d7b16ed1b7b2fb
bsarifhsu <b_sari@mail.fhsu.edu>
2023-11-30 20:45:34 -0600

    API connection added.

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+# myapp/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # URL for the index page
+    path('detail/<int:item_id>/', views.detail, name='detail'),  # URL for item detail with its ID
+]
--- api/views.py
+++ api/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import render
 from .api_keys import NETDETECTIVE_API_KEY
 
 def index(request):
-    # Implement logic to send queries to the NetDetective API
-    # Use NETDETECTIVE_API_KEY in the headers of your API requests
-    # Handle the response data accordingly
-    # Render the index page with the retrieved data
-
     url = "https://netdetective.p.rapidapi.com/query"
     headers = {
         "X-RapidAPI-Key": NETDETECTIVE_API_KEY,
@@ -16,7 +11,34 @@ def index(request):
 
     response = requests.get(url, headers=headers)
 
-    retrieved_data = response.json()
+    if response.status_code == 200:
+        retrieved_data = response.json()['result']
+    else:
+        # Handle API request failure or errors here
+        retrieved_data = {}
+    print(retrieved_data)
     return render(request, 'index.html', {'data': retrieved_data})
 
+def detail(request, item_id):
+    url = "https://netdetective.p.rapidapi.com/query"
+    headers = {
+        "X-RapidAPI-Key": NETDETECTIVE_API_KEY,
+        "X-RapidAPI-Host": "netdetective.p.rapidapi.com"
+    }
 
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        retrieved_data = response.json()['result']
+    else:
+        # Handle API request failure or errors here
+        retrieved_data = []
+
+    # Find the specific item using its ID
+    item = next((item for item in retrieved_data if item.get('id') == item_id), None)
+
+    if not item:
+        # If the item with the specified ID is not found, return a 404 page or handle accordingly
+        return render(request, '404.html')
+
+    return render(request, 'details.html', {'item': item})
