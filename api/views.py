01faf402b36bde3f5e8838be12244cda8ad2f1da
bsarifhsu <b_sari@mail.fhsu.edu>
2023-11-30 20:03:15 -0600

    requirements.txt added.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
+import requests
 from django.shortcuts import render
+from .api_keys import NETDETECTIVE_API_KEY
 
-# Create your views here.
+def index(request):
+    # Implement logic to send queries to the NetDetective API
+    # Use NETDETECTIVE_API_KEY in the headers of your API requests
+    # Handle the response data accordingly
+    # Render the index page with the retrieved data
+
+    url = "https://netdetective.p.rapidapi.com/query"
+    headers = {
+        "X-RapidAPI-Key": NETDETECTIVE_API_KEY,
+        "X-RapidAPI-Host": "netdetective.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    retrieved_data = response.json()
+    return render(request, 'index.html', {'data': retrieved_data})
+
+
