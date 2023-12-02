efa6ef8dbaf6ddda2bed54a0df2aa31e69dd0238
bsarifhsu <b_sari@mail.fhsu.edu>
2023-12-02 14:17:48 -0600

    The API is connected and the data retrieved and displayed at the index.html

--- api/views.py
+++ api/views.py
@@ -2,22 +2,32 @@ import requests
 from django.shortcuts import render
 from .api_keys import NETDETECTIVE_API_KEY
 
+import requests
+from django.shortcuts import render
+from .api_keys import NETDETECTIVE_API_KEY
+
+
 def index(request):
-    url = "https://netdetective.p.rapidapi.com/query"
-    headers = {
-        "X-RapidAPI-Key": NETDETECTIVE_API_KEY,
-        "X-RapidAPI-Host": "netdetective.p.rapidapi.com"
-    }
+    try:
+        url = "https://netdetective.p.rapidapi.com/query"
+        headers = {
+            "X-RapidAPI-Key": NETDETECTIVE_API_KEY,
+            "X-RapidAPI-Host": "netdetective.p.rapidapi.com"
+        }
 
-    response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        retrieved_data = response.json()['result']
-    else:
-        # Handle API request failure or errors here
+        if response.status_code == 200:
+            retrieved_data = response.json()['result']
+        else:
+            retrieved_data = {}
+    except Exception as e:
+        # Log the exception to see what went wrong
+        print(f"Error occurred: {e}")
         retrieved_data = {}
     print(retrieved_data)
-    return render(request, 'index.html', {'data': retrieved_data})
+    return render(request, 'api/index.html', {'data': retrieved_data})
+
 
 def detail(request, item_id):
     url = "https://netdetective.p.rapidapi.com/query"
@@ -39,6 +49,6 @@ def detail(request, item_id):
 
     if not item:
         # If the item with the specified ID is not found, return a 404 page or handle accordingly
-        return render(request, '404.html')
+        return render(request, 'api/404.html')
 
-    return render(request, 'details.html', {'item': item})
+    return render(request, 'api/details.html', {'item': item})
