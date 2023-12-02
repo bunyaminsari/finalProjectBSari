9ea5bf4e169f11a564a391371d6029a21c40266f
bsarifhsu <b_sari@mail.fhsu.edu>
2023-12-02 14:34:49 -0600

    Details Page removed.

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # URL for the index page
-    path('detail/<int:item_id>/', views.detail, name='detail'),  # URL for item detail with its ID
     path('query/', views.query, name='query'),
 ]
--- api/views.py
+++ api/views.py
@@ -29,38 +29,6 @@ def index(request):
     return render(request, 'api/index.html', {'data': retrieved_data})
 
 
-def detail(request, item_id):
-    url = "https://netdetective.p.rapidapi.com/query"
-    headers = {
-        "X-RapidAPI-Key": NETDETECTIVE_API_KEY,
-        "X-RapidAPI-Host": "netdetective.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        retrieved_data = response.json()['result']
-    else:
-        # Handle API request failure or errors here
-        retrieved_data = []
-
-    # Find the specific item using its ID
-    item = next((item for item in retrieved_data if item.get('id') == item_id), None)
-
-    if not item:
-        # If the item with the specified ID is not found, return a 404 page or handle accordingly
-        return render(request, 'api/404.html')
-
-    return render(request, 'api/details.html', {'item': item})
-
-
-# views.py
-
-import requests
-from django.shortcuts import render
-from .api_keys import NETDETECTIVE_API_KEY
-
-
 def query(request):
     if request.method == 'POST':
         ip_address = request.POST.get('ip_address')
